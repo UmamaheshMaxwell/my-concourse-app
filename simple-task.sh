0dd7fa14f9b3d1e6945a268c4ef9a9496a3bc372
Umamaheswararao Meka <uma@olivetech.com>
2019-03-15 17:47:16 +0530

    added keys

--- simple-task.sh
+++ simple-task.sh
@@ -2,5 +2,5 @@ echo "did you call me ?"
 echo "$@"
 key1=$1
 key2=$2
-echo "aws_access_key_id : ${key1}"
-echo "aws_access_key_id : ${key2}"
+echo "aws_access_key_id : $1"
+echo "aws_access_key_id : $2"
