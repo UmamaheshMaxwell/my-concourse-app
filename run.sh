296dce8816cabf5b1db5155977d2295c3fff1e8a
Umamaheswararao Meka <uma@olivetech.com>
2019-03-15 13:03:20 +0530

    passing params to yml

---
+++ run.sh
@@ -0,0 +1,4 @@
+#fly -t turorial login -c http://127.0.0.1:8080
+fly -t turorial set-pipeline -p turorial-pipeline -c ci/pipelines/pipeline.yml
+fly -t turorial unpause-pipeline -p turorial-pipeline
+fly -t turorial trigger-job -w -j turorial-pipeline/first-job
